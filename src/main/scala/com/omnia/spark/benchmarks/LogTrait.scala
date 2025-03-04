a0ac0bd7610070373a83b3293cf8f2f05d444798
Minsu Sun <20790969+minsusun@users.noreply.github.com>
2025-03-04 20:35:01 +0900

    feat(LogTrait): add title support

--- src/main/scala/com/omnia/spark/benchmarks/LogTrait.scala
+++ src/main/scala/com/omnia/spark/benchmarks/LogTrait.scala
@@ -2,10 +2,17 @@ package com.omnia.spark.benchmarks
 
 trait LogTrait {
   private var lastStepTime = System.nanoTime();
+  private var titleString = ""
   private var logString = ""
 
+  def title: String = titleString
+
+  def setTitle(t: String): Unit = {
+    titleString = t
+  }
+
   def step(StepName: String): Unit = {
-    val now = System.nanoTime();
+    val now = System.nanoTime()
     logString += "\n\t\t\t\tStep '" + StepName + s"': ${(now - lastStepTime)/1000000} ms"
     lastStepTime = now
   }
@@ -15,8 +22,9 @@ trait LogTrait {
   }
 
   def forceUpdate(): Unit = {
-    lastStepTime = System.nanoTime();
+    lastStepTime = System.nanoTime()
   }
 
-  def logToString: String = logString;
+  def logToString: String = logString
+  def logToStringWithTitle: String = title + logString
 }
