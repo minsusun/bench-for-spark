984f5722fdacd9478a7cb95aabc6973b1b086a79
Minsu Sun <20790969+minsusun@users.noreply.github.com>
2025-03-04 20:14:40 +0900

    chore: remove legacy(unused)

--- src/main/scala/com/omnia/spark/benchmarks/GraphXHelper.scala
+++
@@ -1,9 +0,0 @@
-package org.apache.spark.graphx {
-
-  import org.apache.spark.graphx.impl.EdgePartitionBuilder
-
-  class GraphXHelper {
-    val instance = new EdgePartitionBuilder[Int, Int]()
-    val method = instance.toEdgePartition
-  }
-}
