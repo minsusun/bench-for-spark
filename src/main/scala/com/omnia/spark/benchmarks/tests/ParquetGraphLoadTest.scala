a3ea173055be7e75911684a1e7022b71cd2d2fc0
Minsu Sun <20790969+minsusun@users.noreply.github.com>
2025-03-05 14:19:52 +0900

    feat: init parquet graph load test

---
+++ src/main/scala/com/omnia/spark/benchmarks/tests/ParquetGraphLoadTest.scala
@@ -0,0 +1,15 @@
+package com.omnia.spark.benchmarks.tests
+
+import com.omnia.spark.benchmarks.{LogTrait, ParseOptions, SQLTest}
+import org.apache.spark.sql.SparkSession
+
+class ParquetGraphLoadTest(val options: ParseOptions, spark:SparkSession) extends SQLTest(spark) with LogTrait{
+
+  override def execute(): String = {
+
+  }
+
+  override def explain(): Unit = ???
+
+  override def plainExplain(): String = ???
+}
