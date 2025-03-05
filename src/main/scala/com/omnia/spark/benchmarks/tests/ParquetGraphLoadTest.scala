37317b9219efd3011b7b05670b7034e538fd2cb4
Minsu Sun <20790969+minsusun@users.noreply.github.com>
2025-03-05 15:17:19 +0900

    feat: add ParquetGraphLoadTest

--- src/main/scala/com/omnia/spark/benchmarks/tests/ParquetGraphLoadTest.scala
+++ src/main/scala/com/omnia/spark/benchmarks/tests/ParquetGraphLoadTest.scala
@@ -1,15 +1,35 @@
 package com.omnia.spark.benchmarks.tests
 
+import com.omnia.spark.benchmarks.graphLoader.ParquetGraphLoader
 import com.omnia.spark.benchmarks.{LogTrait, ParseOptions, SQLTest}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.graphx.AuxGraphLoader
 
 class ParquetGraphLoadTest(val options: ParseOptions, spark:SparkSession) extends SQLTest(spark) with LogTrait{
 
   override def execute(): String = {
-
+    val loaderName = options.getGraphLoader
+    val filePath: String = options.getInputFiles()(0)
+    if (loaderName.compareToIgnoreCase("graphX") == 0 || loaderName.compareToIgnoreCase("aux") == 0) {
+      val loader = new AuxGraphLoader()
+      val graph = loader.edgeListFile(spark.sparkContext, filePath)
+      concatLog(loader.logToString)
+      forceUpdate()
+      val _ = graph.cache()
+      step("[AuxGraphLoader]Graph Cache")
+    } else {
+      assert(filePath.endsWith(".parquet"), "😡 Given file is not parquet format")
+      val loader = new ParquetGraphLoader()
+      val graph = loader.load(spark, filePath)
+      concatLog(loader.logToString)
+      forceUpdate()
+      val _ = graph.cache()
+      step("[ParquetGraphLoader]Graph Cache");
+    }
+    s"Ran Parquet GraphLoadTest(Loader: ${loaderName}" + logToString
   }
 
-  override def explain(): Unit = ???
+  override def explain(): Unit = println(plainExplain())
 
-  override def plainExplain(): String = ???
+  override def plainExplain(): String = "ParquetGraphLoadTest"
 }
