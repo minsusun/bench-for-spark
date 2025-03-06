b1123e3cf119194af099633d626d904c8728e13b
Minsu Sun <20790969+minsusun@users.noreply.github.com>
2025-03-06 15:03:14 +0900

    feat(PageRank): implement new pagerank based on test graph loader

--- src/main/scala/com/omnia/spark/benchmarks/tests/PageRank.scala
+++ src/main/scala/com/omnia/spark/benchmarks/tests/PageRank.scala
@@ -1,13 +1,25 @@
 package com.omnia.spark.benchmarks.tests
 
+import com.omnia.spark.benchmarks.graphLoader.TestGraphLoader
 import com.omnia.spark.benchmarks.{LogTrait, ParseOptions, SQLTest}
 import org.apache.spark.sql.SparkSession
 
 class PageRank (val options: ParseOptions, spark: SparkSession) extends SQLTest(spark) with LogTrait {
 
-  override def execute(): String = ???
+  override def execute(): String = {
+    val loader = new TestGraphLoader(options, spark);
+    val graph = loader.load()
 
-  override def explain(): Unit = ???
+    concatLog(loader.explain)
+    forceUpdate()
 
-  override def plainExplain(): String = ???
+    org.apache.spark.graphx.lib.PageRank.run(graph, options.getPageRankIterations)
+    step("[PageRank]Execution")
+
+    "Ran PageRank " + options.getPageRankIterations + " iterations on " + options.getInputFiles()(0) + logToString
+  }
+
+  override def explain(): Unit = println(plainExplain())
+
+  override def plainExplain(): String = ""
 }
