37bbc400fb342d9231f62e409ec40a716ca96aa0
Minsu Sun <20790969+minsusun@users.noreply.github.com>
2025-03-13 22:02:43 +0900

    feat(ConnectedComponents): add time measurement

--- src/main/scala/com/omnia/spark/benchmarks/tests/ConnectedComponents.scala
+++ src/main/scala/com/omnia/spark/benchmarks/tests/ConnectedComponents.scala
@@ -1,36 +1,31 @@
 package com.omnia.spark.benchmarks.tests
 
-import com.omnia.spark.benchmarks.{Action, Noop, ParseOptions, SQLTest}
-import org.apache.spark.graphx.GraphLoader
+import com.omnia.spark.benchmarks.graphLoader.TestGraphLoader
+import com.omnia.spark.benchmarks.{LogTrait, ParseOptions, SQLTest}
 import org.apache.spark.sql.SparkSession
 
-class ConnectedComponents (val options: ParseOptions, spark:SparkSession) extends SQLTest(spark) {
-  /* we got the input file, we then load it */
-  private val start = System.nanoTime()
-  private val graph = GraphLoader.edgeListFile(spark.sparkContext, options.getInputFiles()(0)).cache()
-  private val end = System.nanoTime()
+class ConnectedComponents (val options: ParseOptions, spark:SparkSession) extends SQLTest(spark) with LogTrait{
 
   override def execute(): String = {
-    /* we don't have to take any action from the datasets */
-    val result = org.apache.spark.graphx.lib.ConnectedComponents.run(this.graph, options.getPageRankIterations)
-    result.vertices.coalesce(1).saveAsTextFile("dbg/resultVE")
-    result.edges.coalesce(1).saveAsTextFile("dbg/resultEG")
+    val loader = new TestGraphLoader(options, spark)
+    val graph = loader.load()
 
-    "Ran ConnectedComponents " + options.getPageRankIterations + " iterations on " + options.getInputFiles()(0)
+    concatLog(loader.explain)
+    forceUpdate()
+
+    val result = org.apache.spark.graphx.lib.ConnectedComponents.run(graph, options.getPageRankIterations)
+    step("[ConnectedComponents]Execution")
+
+    result.vertices.coalesce(1).saveAsTextFile("dbg/resultVE")
+    step("[ConnectedComponents]Save resultVE")
+
+    result.edges.coalesce(1).saveAsTextFile("dbg/resultEG")
+    step("[ConnectedComponents]Save resultEG")
+
+    "Ran ConnectedComponents " + options.getPageRankIterations + " iterations on " + options.getInputFiles()(0) + logToString
   }
 
   override def explain(): Unit = println(plainExplain())
 
   override def plainExplain(): String = "ConnectedComponents " + options.getPageRankIterations + " iterations on " + options.getInputFiles()(0)
-
-  override def printAdditionalInformation():String = {
-    val sb = new StringBuilder()
-    sb.append("Graph load time: " + (end - start)/1000000 + " msec\n")
-    val str = options.getAction match {
-      case noop:Noop => ""
-      case a:Action => "Warning: action " + a.toString + " was ignored. PageRank does not need any explicit action.\n"
-    }
-    sb.append(str)
-    sb.mkString
-  }
 }
